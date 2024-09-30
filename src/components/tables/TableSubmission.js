ea52c3433f6ce8abd8496e8c30df8f7c7ad3a5bc
Fahrizal <m.fahrizalalipradana@gmail.com>
2024-09-30 17:20:07 +0700

    update column setup for table

--- src/components/tables/TableSubmission.js
+++ src/components/tables/TableSubmission.js
@@ -5,10 +5,10 @@ import {
   GridToolbarColumnsButton,
   GridToolbarFilterButton,
   GridToolbarExport,
-  GridToolbarDensitySelector,
-} from '@mui/x-data-grid';
+  GridToolbarDensitySelector
+} from "@mui/x-data-grid";
 import Paper from "@mui/material/Paper";
-import { Box } from "@mui/material";
+import { Box} from "@mui/material";
 
 function CustomToolbar() {
   return (
@@ -16,57 +16,101 @@ function CustomToolbar() {
       <GridToolbarColumnsButton />
       <GridToolbarFilterButton />
       <GridToolbarDensitySelector
-        slotProps={{ tooltip: { title: 'Change density' } }}
+        slotProps={{ tooltip: { title: "Change density" } }}
       />
       <Box sx={{ flexGrow: 1 }} />
       <GridToolbarExport
         slotProps={{
-          tooltip: { title: 'Export data' },
+          tooltip: { title: "Export data" },
         }}
       />
     </GridToolbarContainer>
   );
 }
 
-const columns = [
-  { field: "SubmissionID", headerName: "ID", width:40},
-  { field: "StudentID", headerName: "NIM", width: 110},
-  { field: "ProdiName", headerName: "Prodi", width: 150},
-  { field: "SubmissionDate", headerName: "Submit Date", width: 130},
-  { field: "ProgramType", headerName: "Program Type", width: 180},
-  { field: "Title", headerName: "Title", width: 200},
-  { field: "InstitutionName", headerName: "Institution Name", width: 150},
-  { field: "Position", headerName: "Position", width: 130},
-  { field: "ApprovalStatus", headerName: "Status", width: 180},
-]
-
-const paginationModel = { page: 0, pageSize:10};
+const paginationModel = { page: 0, pageSize: 10 };
 
 export default function TableSubmission() {
   const [submissions, setSubmissions] = React.useState([]);
   const [selectedRows, setSelectedRows] = React.useState([]);
+  const [columnVisibilityModel, setColumnVisibilityModel] = useState();
+  const [columns, setColumns] = React.useState([]);
+
+  const handleColumnVisibilityChange = (newModel) => {
+    console.log(
+      "Kolom yang terlihat:",
+      Object.keys(newModel).filter((key) => newModel[key])
+    );
+    console.log(
+      "Kolom yang tersembunyi:",
+      Object.keys(newModel).filter((key) => !newModel[key])
+    );
+    setColumnVisibilityModel(newModel);
+  };
 
   useEffect(() => {
     async function getBreadcrumb() {
       try {
-        const response = await fetch("http://localhost:3001/api/submission");
+        const response = await fetch(
+          "http://localhost:3001/api/pending-submission/2"
+        );
         const data = await response.json();
         setSubmissions(data);
       } catch (error) {
         console.error("Error fetching menu:", error);
-      } 
+      }
+    }
+    async function getColumn() {
+      try {
+        const response = await fetch(
+          "http://localhost:3001/api/column/Submission/2"
+        );
+        const data = await response.json();
+        setColumns(data.column);
+        setColumnVisibilityModel(data.visibility);
+      } catch (error) {
+        console.error("Error fetching menu:", error);
+      }
     }
     getBreadcrumb();
+    getColumn();
   }, []);
 
+  const debounce = (func, delay) => {
+    let timeout;
+    return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+  const handleColumnResizeCommitted = debounce((params) => {
+    console.log(
+      `Column ${params.colDef.field} finalized resize to: ${params.width}`
+    );
+
+    setColumns((prevColumns) =>
+      prevColumns.map((col) =>
+        col.field === params.colDef.field
+          ? { ...col, width: params.width }
+          : col
+      )
+    );
+
+    const col = columns.filter((col) => col.field === params.colDef.field)[0];
+
+    
+  }, 300);
 
   const handleSelectionChange = (newSelectionModel) => {
-    console.log('Selected Rows:', newSelectionModel);
+    console.log("Selected Rows:", newSelectionModel);
     setSelectedRows(newSelectionModel);
   };
 
   return (
-    <Paper sx={{ maxHeight: '100vh'}}>
+    <Paper sx={{ maxHeight: "100vh" }}>
       <DataGrid
         rows={submissions}
         columns={columns}
@@ -74,11 +118,16 @@ export default function TableSubmission() {
         getRowId={(row) => row.SubmissionID}
         pageSizeOptions={[5, 10]}
         checkboxSelection
-        onSelectionModelChange={(newSelectionModel) => handleSelectionChange(newSelectionModel)}
-        selectionModel={selectedRows}  
+        onSelectionModelChange={(newSelectionModel) =>
+          handleSelectionChange(newSelectionModel)
+        }
+        selectionModel={selectedRows}
         slots={{
           toolbar: CustomToolbar,
         }}
+        columnVisibilityModel={columnVisibilityModel}
+        onColumnVisibilityModelChange={handleColumnVisibilityChange}
+        onColumnResize={handleColumnResizeCommitted}
       />
     </Paper>
   );
