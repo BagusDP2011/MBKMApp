564c2308aeffa9e17b69699483e1fd21bbf0d409
Bagus Dwi Putra <bagusdp2011@gmail.com>
2024-10-24 18:28:30 +0700

    Added page buatan akmal dan yusti compare

--- src/mapItem/mapItem.js
+++ src/mapItem/mapItem.js
@@ -3,6 +3,7 @@ import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
 import SegmentOutlinedIcon from '@mui/icons-material/SegmentOutlined';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
 import ListSubmission from "../pages/menu/Submission/ListSubmission";
 import Submission from "../pages/menu/Submission/Submission";
 import PengajuanMHS from "../pages/menu/PengajuanMHS";
@@ -13,7 +14,8 @@ export const iconsMap = {
     "<DashboardOutlinedIcon />" : <DashboardOutlinedIcon sx={{fontSize:'1.7rem'}}/>,
     "<GroupsOutlinedIcon />" : <GroupsOutlinedIcon sx={{fontSize:'1.7rem'}}/>,
     "<InfoOutlinedIcon />" : <InfoOutlinedIcon sx={{fontSize:'1.7rem'}}/>,
-    "<DescriptionOutlinedIcon />" : <DescriptionOutlinedIcon sx={{fontSize:'1.7rem'}}/>
+    "<DescriptionOutlinedIcon />" : <DescriptionOutlinedIcon sx={{fontSize:'1.7rem'}}/>,
+    "<LocalLibraryIcon />" : <LocalLibraryIcon sx={{fontSize:'1.7rem'}}/>,
   };
   
   export const componentsMap = {
