4789cf2da52e2923ccbe1359b1fa4296ce135e37
Fahrizal <m.fahrizalalipradana@gmail.com>
2024-10-25 13:38:39 +0700

    Add pages Attach,Information

--- src/mapItem/mapItem.js
+++ src/mapItem/mapItem.js
@@ -6,6 +6,9 @@ import SegmentOutlinedIcon from '@mui/icons-material/SegmentOutlined';
 import ListSubmission from "../pages/menu/Submission/ListSubmission";
 import Submission from "../pages/menu/Submission/Submission";
 import DetailSubmission from "../pages/menu/Submission/DetailSubmission";
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import AttachmentSubmission from "../pages/menu/Submission/AttachmentSubmission";
+import SubmissionInformation from "../pages/menu/Submission/SubmissionInformation";
 
 export const iconsMap = {
     "<SegmentOutlinedIcon />" : <SegmentOutlinedIcon sx={{fontSize:'1.7rem'}}/>,
@@ -13,10 +16,13 @@ export const iconsMap = {
     "<GroupsOutlinedIcon />" : <GroupsOutlinedIcon sx={{fontSize:'1.7rem'}}/>,
     "<InfoOutlinedIcon />" : <InfoOutlinedIcon sx={{fontSize:'1.7rem'}}/>,
     "<DescriptionOutlinedIcon />" : <DescriptionOutlinedIcon sx={{fontSize:'1.7rem'}}/>,
+    "<AttachFileIcon />" : <AttachFileIcon sx={{fontSize:'1.7rem'}}/>,
   };
   
   export const componentsMap = {
     "<ListSubmission />" : ListSubmission,
     "<Submission />" : Submission,
-    "<DetailSubmission />" : DetailSubmission
+    "<DetailSubmission />" : DetailSubmission,
+    "<AttachmentSubmission />" : AttachmentSubmission,
+    "<SubmissionInformation />" : SubmissionInformation,
   };
